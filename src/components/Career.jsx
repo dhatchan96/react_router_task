fb780f4b661c3f94a715d5b61338eecb8a89087e
dhatchan96 <dhatchan0@gmail.com>
2024-09-02 17:33:10 +0530

    updated image and all page

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,21 +1,22 @@
-
-
 const Career = () => {
   return (
     <div>
-    <h2>Career</h2>
-    <div className="courses">      
-      <div className="course-card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMpT81z7GrJmCMjEy5qZwknokxwfbJzoFF_w&s" />
-                    <p>10 Hardest and Easiest Programming Languages in 2024</p>
+      <h2>Career</h2>
+      <div className="courses">
+        <div className="course-card">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMpT81z7GrJmCMjEy5qZwknokxwfbJzoFF_w&s" />
+          <p>10 Hardest and Easiest Programming Languages in 2024</p>
+        </div>
+        <div className="course-card">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbzw5HiLkO-BRSx64XSVbgmWLTMyrIeVc6NA&s" />
+          <p>
+            30 Best JavaScript Project Ideas For You [3 Bonus Portfolio
+            Projects]
+          </p>
+        </div>
       </div>
-      <div className="course-card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbzw5HiLkO-BRSx64XSVbgmWLTMyrIeVc6NA&s" />
-                    <p>30 Best JavaScript Project Ideas For You [3 Bonus Portfolio Projects]</p>
-      </div>
-    </div>
     </div>
   );
-}
+};
 
 export default Career;
