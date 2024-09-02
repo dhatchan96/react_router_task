be44ef941762608706eb3fc4ad401d0a3c956273
dhatchan96 <dhatchan0@gmail.com>
2024-09-02 17:23:07 +0530

    updated image

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -6,11 +6,11 @@ const Career = () => {
     <h2>Career</h2>
     <div className="courses">      
       <div className="course-card">
-                    <img src="https://www.guvi.in/blog/wp-content/uploads/2024/08/Roadmap-For-Generative-AI.png" />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMpT81z7GrJmCMjEy5qZwknokxwfbJzoFF_w&s" />
                     <p>10 Hardest and Easiest Programming Languages in 2024</p>
       </div>
       <div className="course-card">
-                    <img src="https://www.guvi.in/blog/wp-content/uploads/2023/02/animated-javascript-project-image.jpg" />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbzw5HiLkO-BRSx64XSVbgmWLTMyrIeVc6NA&s" />
                     <p>30 Best JavaScript Project Ideas For You [3 Bonus Portfolio Projects]</p>
       </div>
     </div>
